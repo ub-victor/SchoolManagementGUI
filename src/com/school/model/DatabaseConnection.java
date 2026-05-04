465ec86761e4fd3427c6cb9bc9b7cd2512365877
ub-victor <victoireushindi371@gmail.com>
2026-05-04 19:47:10 +0200

    Db connection added

---
+++ src/com/school/model/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.school.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/school_db";
+    private static final String USER = "root";   // your MySQL username
+    private static final String PASSWORD = "";   // your MySQL password (empty by default on some Ubuntu setups)
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
