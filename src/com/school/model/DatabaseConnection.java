b05535b1c0308171eea5237c6f70e314015c41c7
ub-victor <victoireushindi371@gmail.com>
2026-05-04 20:09:42 +0200

    add the connection

--- src/com/school/model/DatabaseConnection.java
+++ src/com/school/model/DatabaseConnection.java
@@ -8,7 +8,6 @@ public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/school_db";
     private static final String USER = "root";
     private static final String PASSWORD = "Ushindi123!";
-
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
