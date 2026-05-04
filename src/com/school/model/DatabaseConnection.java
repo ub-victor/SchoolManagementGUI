643c59e1d69564c696cf213f48694594f8c034f4
ub-victor <victoireushindi371@gmail.com>
2026-05-04 19:50:49 +0200

    Db connection added

--- src/com/school/model/DatabaseConnection.java
+++ src/com/school/model/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/school_db";
-    private static final String USER = "root";   // your MySQL username
+    private static final String USER = "root";
     private static final String PASSWORD = "Ushindi123!";
 
     public static Connection getConnection() throws SQLException {
