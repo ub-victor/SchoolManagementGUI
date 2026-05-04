338b43203622ce76ded769c2f4710d130d025a67
ub-victor <victoireushindi371@gmail.com>
2026-05-04 20:08:52 +0200

    add the logic to the main frame

--- src/com/school/view/MainFrame.java
+++ src/com/school/view/MainFrame.java
@@ -1,16 +1,16 @@
 package com.school.view;
 
-import javax.swing.*;
 import com.school.controller.SchoolController;
+import javax.swing.*;
 
 public class MainFrame extends JFrame {
     private SchoolController controller;
 
     public MainFrame() {
         controller = new SchoolController();
-        setTitle("School Management System");
+        setTitle("School Management System - MVC + MySQL");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900, 600);
+        setSize(1000, 700);
         setLocationRelativeTo(null);
 
         JTabbedPane tabbedPane = new JTabbedPane();
@@ -18,9 +18,13 @@ public class MainFrame extends JFrame {
         tabbedPane.addTab("Teachers", new TeacherPanel(controller));
         tabbedPane.addTab("Courses", new CoursePanel(controller));
         tabbedPane.addTab("Enrollments", new EnrollmentPanel(controller));
-        tabbedPane.addTab("Pay Fees", new PaymentPanel(controller));
+        tabbedPane.addTab("Fee Payments", new PaymentPanel(controller));
 
         add(tabbedPane);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MainFrame());
     }
 }
